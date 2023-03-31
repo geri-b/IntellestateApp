296c31064d5db5c037d1c0a3bc649b0b9e80012c
drisch314 <drisch2010@gmail.com>
2023-03-31 02:04:57 -0400

    Add test map. Update card and button styling

--- intellestate-app/src/components/PropertyDetails.js
+++ intellestate-app/src/components/PropertyDetails.js
@@ -1,9 +1,44 @@
 import Plot from "react-plotly.js";
+import Map, { Layer, Source, Marker, Popup } from 'react-map-gl';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+const geojson = {
+  type: 'FeatureCollection',
+  features: [
+    {type: 'Feature', geometry: {type: 'Point', coordinates: [-81.9151, 41.4816]}}
+  ]
+};
+
+const layerStyle = {
+  id: 'point',
+  type: 'circle',
+  paint: {
+    'circle-radius': 5,
+    'circle-color': '#007cbf'
+  }
+};
 
 function PropertyDetails() {
   return (
     <div style={{ width: "100%" }}>
       <h3>Visualizations</h3>
+      <br></br>
+      <Map
+        initialViewState={{
+          longitude: -81.6,
+          latitude: 41.5,
+          zoom: 8
+        }}
+        style={{width: '100%', aspectRatio: '3/2', border: '2px solid lightgrey', borderRadius: '4px'}}
+        mapLib={maplibregl}
+        mapStyle="https://api.maptiler.com/maps/bright/style.json?key=nmF5UJHGt6DxUo6Ooheo"
+        type='vector'
+      >
+        <Source id="my-data" type="geojson" data={geojson}>
+          <Layer {...layerStyle}></Layer>
+        </Source>
+      </Map>
       <br></br>
       <Plot
         data={[
