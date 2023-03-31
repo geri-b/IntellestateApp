be4a55a6ab83b7ef9d2a83d1b58780b76fee4d5e
geri18-coder <gbakushi@gmail.com>
2023-03-31 19:24:54 -0400

    Added data to the property cards and did some style changes

--- intellestate-app/src/components/Navibar.js
+++ intellestate-app/src/components/Navibar.js
@@ -4,15 +4,15 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function Navibar({ handlePageChange }) {
     return (
-        <Navbar bg="light" expand="lg" style={{ padding: "10px" }}>
+        <Navbar bg="light" expand="lg">
             <Navbar.Brand href="#home">Intellestate</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link onClick={() => handlePageChange("home")}>
+                    <Nav.Link onClick={() => handlePageChange("home")}  style = {{ color: "#007BFF" }}>
                         Home
                     </Nav.Link>
-                    <Nav.Link onClick={() => handlePageChange("browse")}>
+                    <Nav.Link onClick={() => handlePageChange("browse")} style = {{ color: "#007BFF" }}>
                         Browse Properties
                     </Nav.Link>
                 </Nav>
