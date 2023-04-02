0d40b8d686f340b4273c79e660b4b11b5c00bfa8
Gabe Phillips <93894270+GabePhillips@users.noreply.github.com>
2023-04-02 17:16:57 -0400

    Finished sorting from backend, Small QOL changes

--- intellestate-app/src/components/filterToolComponents/CheckBox.js
+++ intellestate-app/src/components/filterToolComponents/CheckBox.js
@@ -1,29 +1,35 @@
 import React from 'react';
+import { Col } from 'react-bootstrap';
 
 function Checkbox({ label, name, checked, onChange, weight, onWeightChange }) {
     return (
         <div className="form-check d-flex align-items-center">
-            <input
-                className="form-check-input"
-                type="checkbox"
-                name={name}
-                id={name}
-                checked={checked}
-                onChange={onChange}
-            />
-            <label className="form-check-label" htmlFor={name}>
-                {label}
-            </label>
-            <input
-                type="number"
-                className="form-control ml-2"
-                name={`${name}_weight`}
-                id={`${name}_weight`}
-                value={weight}
-                onChange={onWeightChange}
-                disabled={!checked}
-                style={{ width: "80px" }}
-            />
+            <Col className="d-flex justify-content-start">
+                <input
+                    className="form-check-input"
+                    type="checkbox"
+                    name={name}
+                    id={name}
+                    checked={checked}
+                    onChange={onChange}
+                />
+
+                <label className="form-check" htmlFor={name}>
+                    {label}
+                </label>
+            </Col>
+            <Col>
+                <input
+                    type="number"
+                    className="form-control ml-2"
+                    name={`${name}_weight`}
+                    id={`${name}_weight`}
+                    value={weight}
+                    onChange={onWeightChange}
+                    disabled={!checked}
+                    style={{ width: "80px" }}
+                />
+            </Col>
         </div>
     );
 }
--- intellestate-app/src/components/filterToolComponents/ConfirmSearchButton.js
+++ intellestate-app/src/components/filterToolComponents/ConfirmSearchButton.js
@@ -1,11 +1,18 @@
 import Button from 'react-bootstrap/Button';
 
-function ConfirmSearchButton({onSearchClick}) {
+function ConfirmSearchButton({onSearchClick, searchInProgress}) {
 
 
   return (
     <>
-      <Button style={{maxWidth: '200px'}} variant="primary" onClick={onSearchClick}>Search</Button>{''}
+      <Button 
+        style={{maxWidth: '200px'}} 
+        variant="primary" 
+        onClick={onSearchClick}
+        disabled={searchInProgress} // Disable the button when search is in progress
+        >
+          {searchInProgress ? "Searching..." : "Search"}
+      </Button>{''}
     </>
   );
 }
