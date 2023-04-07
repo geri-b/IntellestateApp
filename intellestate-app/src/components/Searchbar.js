f591b604cf93ee338cf261b7845005e7ed7eb085
Gabe Phillips <93894270+GabePhillips@users.noreply.github.com>
2023-04-07 19:27:27 -0400

    Changes

--- intellestate-app/src/components/Searchbar.js
+++ intellestate-app/src/components/Searchbar.js
@@ -3,10 +3,16 @@ import { Form, FormControl, Button } from "react-bootstrap";
 
 function SearchBar() {
   return (
-    <Form inline>
+    <Form inline style={{
+      position: 'static',
+      top: '50%',
+      left: '50%',
+      zIndex: '1',
+      width: '400px'
+    }}>
       <FormControl type="text" placeholder="Enter an address, or zipcode" className="mr-sm-2"/>
-        <Button variant="outline-info">Search</Button>
-    </Form>
+        <Button variant="outline-info" className="my-2">Search</Button>
+    </Form >
   );
 }
 
